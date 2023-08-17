ed0313d14ce8fc8862f4d8e038728e59f7b80b5b
pamelapose04 <142383279+pamelapose04@users.noreply.github.com>
2023-08-17 17:41:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,11 @@ fruityvice_normalized = pandas. json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit. dataframe(fruityvice_normalized)
 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
 
 
 
